cea73dc8740d205a44e8f726ff434fcec538b698
segf00lt <joaodear2007@gmail.com>
2022-06-18 17:20:47 -0300

    reverse polish calculator exercises in chapter4 are buggy but I can't be arsed to fix them right now

---
+++ chapter4/ex10.c
@@ -0,0 +1,157 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+
+#define MAXLEN 100
+#define NUMBER 0
+#define ZERODIV 1
+#define DOMAINERR 2
+
+int getch(void);
+void ungetch(int c);
+int getop(char s[]);
+void push(double f);
+double pop(void);
+
+char buf[MAXLEN];
+int bufp = 0;
+
+int sp = 0;
+double val[MAXLEN];
+
+int getch(void) {
+	return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) {
+	if(bufp >= MAXLEN)
+		printf("ungetch: too many characters\n");
+	else
+		buf[bufp++] = c;
+}
+
+int getop(char s[]) {
+	int i, c;
+
+	while((*s = c = getch()) == ' '|| c == '\t');
+	s[1] = '\0';
+
+	if(!isdigit(c) && c != '.')
+		return c;
+
+	i = 0;
+	if(isdigit(c))
+		while(isdigit(s[++i] = c = getch()));
+	if(c == '.')                               
+		while(isdigit(s[++i] = c = getch()));
+	s[i] = '\0';
+
+	if(c != EOF)
+		ungetch(c);
+
+	return NUMBER;
+}
+
+void push(double f) {
+	if(sp < MAXLEN)
+		val[sp++] = f;
+	else
+		printf("error: stack full, can't push %g\n", f);
+}
+
+double pop(void) {
+	if(sp > 0)
+		return val[--sp];
+
+	printf("error: stack empty\n");
+	return 0.0;
+}
+
+int main(void) {
+	int type;
+	double op2, f;
+	char s[MAXLEN];
+	int error = 0;
+
+	while((type = getop(s)) != EOF) {
+		switch(type) {
+		case NUMBER:
+			push(atof(s));
+			break;
+		case '_': /* peek */
+			printf("=>  %.8g\n", (f = pop()));
+			push(f);
+			error = -1;
+			break;
+		case '@': /* swap */
+			f = pop();
+			op2 = pop();
+			push(f);
+			push(op2);
+			error = -1;
+			break;
+		case '#': /* clear */
+			while(sp > 0) --sp;
+			error = -1;
+			break;
+		case 'S':
+			push(sin(pop()));
+			break;
+		case 'E':
+			push(exp(pop()));
+			break;
+		case '^':
+			op2 = pop();
+			f = pop();
+			if((f == 0 && op2 <= 0) || (f < 0 && op2 != floor(op2)))
+				error = DOMAINERR;
+			else
+				push(pow(f, op2));
+			break;
+		case '+':
+			push(pop() + pop());
+			break;
+		case '*':
+			push(pop() * pop());
+			break;
+		case '-':
+			op2 = pop();
+			push(pop() - op2);
+		case '/':
+			op2 = pop();
+			if(op2 != 0.0)
+				push(pop() / op2);
+			else
+				error = ZERODIV;
+			break;
+		case '%':
+			op2 = pop();
+			if(op2 != 0.0)
+				push(fmod(pop(), op2));
+			else
+				error = ZERODIV;
+			break;
+		case '\n':
+			switch(error) {
+			case 0:
+				printf("=>  %.8g\n", pop());
+				break;
+			case ZERODIV:
+				printf("error: zero divisor\n");
+				break;
+			case DOMAINERR:
+				printf("error: domain error\n");
+				break;
+			}
+
+			error = 0;
+			break;
+		default:
+			printf("error: unkown command\n");
+			break;
+		}
+	}
+}
+
+
---
+++ chapter4/ex11.c
@@ -0,0 +1,173 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+
+#define MAXLEN 100
+#define NUMBER 0
+#define ZERODIV 1
+#define DOMAINERR 2
+
+int getch(void);
+void ungetch(int c);
+int getop(char s[]);
+void push(double f);
+int pop(double *f);
+
+char buf[MAXLEN];
+
+int sp = 0;
+double val[MAXLEN];
+
+int getch(void) {
+	int c = getchar();
+
+	if(c == EOF)
+		exit(0);
+
+	return (bufp > 0) ? buf[--bufp] : c;
+}
+
+void ungetch(int c) {
+	if(bufp >= MAXLEN)
+		printf("ungetch: too many characters\n");
+	else
+		buf[bufp++] = c;
+}
+
+int getop(char s[]) {
+	static int bufp = 0;
+	int i, c;
+
+	while((*s = c = getch()) == ' '|| c == '\t');
+	s[1] = '\0';
+
+	if(!isdigit(c) && c != '.')
+		return c;
+
+	i = 0;
+	if(isdigit(c))
+		while(isdigit(s[++i] = c = getch()));
+	if(c == '.')                               
+		while(isdigit(s[++i] = c = getch()));
+	s[i] = '\0';
+
+	if(c != EOF) {
+		if(bufp >= MAXLEN)
+			printf("too many characters in buf\n");
+		else
+			buf[bufp++] = c;
+	}
+
+	return NUMBER;
+}
+
+void push(double f) {
+	if(sp < MAXLEN)
+		val[sp++] = f;
+	else
+		printf("error: stack full, can't push %g\n", f);
+}
+
+int pop(double *f) {
+	if(sp > 0) {
+		*f = val[--sp];
+		return 1;
+	}
+
+	printf("error: stack empty\n");
+	return 0;
+}
+
+int main(void) {
+	int type;
+	double op2, f;
+	char s[MAXLEN];
+	int error = 0;
+
+	while((type = getop(s)) != EOF) {
+		error = 0;
+
+		switch(type) {
+		case NUMBER:
+			push(atof(s));
+			break;
+		case '_': /* peek */
+			if(pop(&f)) {
+				printf("=>  %.8g\n", f);
+				push(f);
+			}
+			break;
+		case '@': /* swap */
+			pop(&op2);
+			pop(&f);
+			push(op2);
+			push(f);
+			break;
+		case '#': /* clear */
+			while(sp > 0) --sp;
+			break;
+		case 'S':
+			pop(&f);
+			push(sin(f));
+			break;
+		case 'E':
+			pop(&f);
+			push(exp(f));
+			break;
+		case '^':
+			pop(&op2);
+			pop(&f);
+			if((f == 0 && op2 <= 0) || (f < 0 && op2 != floor(op2)))
+				error = DOMAINERR;
+			else
+				push(pow(f, op2));
+			break;
+		case '+':
+			pop(&op2);
+			pop(&f);
+			push(f + op2);
+			break;
+		case '*':
+			pop(&op2);
+			pop(&f);
+			push(f * op2);
+			break;
+		case '-':
+			pop(&op2);
+			pop(&f);
+			push(f - op2);
+		case '/':
+			pop(&op2);
+			pop(&f);
+			if(op2 != 0.0)
+				push(f / op2);
+			else
+				error = ZERODIV;
+			break;
+		case '%':
+			pop(&op2);
+			pop(&f);
+			if(op2 != 0.0)
+				push(fmod(f, op2));
+			else
+				error = ZERODIV;
+			break;
+		case '\n':
+			switch(error) {
+			case ZERODIV:
+				printf("error: zero divisor\n");
+				break;
+			case DOMAINERR:
+				printf("error: domain error\n");
+				break;
+			}
+			break;
+		default:
+			printf("error: unkown command\n");
+			break;
+		}
+	}
+}
+
+
